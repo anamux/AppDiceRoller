58bd8ec28846828298dbecf4dc2c0aa61e71d4f0
anamux <anaamux@gmail.com>
2023-11-28 16:27:10 -0300

    Aligned layout

--- app/src/main/java/br/com/ana/appdiceroller/MainActivity.kt
+++ app/src/main/java/br/com/ana/appdiceroller/MainActivity.kt
@@ -31,28 +31,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppDiceRollerTheme {
-
-                DiceWithButtonAndImage()
+                Surface (
+                    modifier = Modifier.fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceWithButtonAndImage()
+                }
             }
         }
 
     }
-    }
+}
 
 
-@Preview( showSystemUi = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center))
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 
 }
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1)    }
-    val imageResource = when (result){
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -70,7 +77,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = {result = (1..6).random() }) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
 
